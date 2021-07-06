56363edfb37c6e7c4a1051017bfb3eea0949ac47
Joseph Goodrick <josephsemail01@gmail.com>
2021-07-05 19:23:45 -0500

    removed commented out irrelevant test code

--- Sources/CodexCore/KeyPathExtensions.swift
+++ Sources/CodexCore/KeyPathExtensions.swift
@@ -8,9 +8,4 @@ internal extension Collection {
     func sorted<U: Comparable>(by keyPath: KeyPath<Element, U>) -> [Element] {
         sorted(by: { $0[keyPath: keyPath] < $1[keyPath: keyPath]} )
     }
-
-    /// This works, but doesn't actually save any code. It is debatable if this is worth using
-//    func filter<T: Comparable>(_ keyPath: KeyPath<Element, T>, _ comparator: (T, T) -> Bool, _ value: T) -> [Element] {
-//        filter({comparator($0[keyPath: keyPath], value)})
-//    }
 }
