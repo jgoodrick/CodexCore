5316f93e76954e471dd8d342ee63becd5c653bb0
Joseph Goodrick <19720019+jgoodrick@users.noreply.github.com>
2021-07-05 19:23:45 -0500

    removed commented out irrelevant test code

--- Tests/CodexCoreTests/KeyPathExtensionTests.swift
+++ Tests/CodexCoreTests/KeyPathExtensionTests.swift
@@ -25,22 +25,3 @@ class KeyPathSortTests: XCTestCase {
         XCTAssertEqual(sut.sorted(by: \.self), sut.sorted())
     }
 }
-
-//class KeyPathFilterTests: XCTestCase {
-//
-//    let item1 = "1"
-//    let item2 = "2"
-//    let item3 = "3"
-//    var items: [String] { [item1, item2, item3] }
-//
-//    func test_nullFilter_returnsUnchanged() {
-//        let sut: [String] = items
-//        XCTAssertEqual(sut.filter(\.self, { _, _ in true }, ""), items)
-//    }
-//
-//    func test_filter_empty_gateFilter_returnsEmpty() {
-//        let sut: [String] = items
-//        XCTAssertEqual(sut.filter(\.self, { _, _ in false }, ""), [])
-//    }
-//
-//}
